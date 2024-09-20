f34b6c8a91b27992bddffb818aad92d609f4a425
Red-Pillow <sahu5chetan5@gmail.com>
2024-09-20 16:57:30 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const app = express(); //call the express to use it handy instance of an expressjs application
 
-app.use('/',(req,res)=>{
-    res.send('hello from the dashboard')
-})
-app.use('/test',(req,res)=>{
-  res.send('hello from the server') // shows in the port 3000
-})
+
+// app.use('/test',(req,res)=>{
+//   res.send('hello from the server') // shows in the port 3000
+// })
 
     
 
-app.use('/hello',(req,res)=>{
-    res.send('hello hello hello!!!')
+// app.use('/hello',(req,res)=>{
+//     res.send('hello hello hello!!!')
+// })
+
+// app.use('/',(req,res)=>{
+//   res.send('hello from the dashboard')
+// })
+
+app.get("/user/:userId/:name/:password",(req,res)=>{
+  console.log(req.params)
+  res.send({firstname:"chetan", lastname: "sahu"})
 })
+
+app.post("/user",(req,res)=>{
+  res.send("data is successfully saved in the DB")
+})
+
 app.listen(3000,()=>{
 console.log("server is succersssfully listening in the port no 3000")
 })
