c4c74b6b0a3a417cca6680beb493b799c41966f5
Red-Pillow <sahu5chetan5@gmail.com>
2024-09-20 17:29:23 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,28 +2,16 @@ const express = require('express');
 const app = express(); //call the express to use it handy instance of an expressjs application
 
 
-// app.use('/test',(req,res)=>{
-//   res.send('hello from the server') // shows in the port 3000
-// })
+app.use("/user", (req,res,next)=>{
+  console.log("handling route user 1")
+  res.send("Response 1");
+  next()
+},(req,res)=>{
+  console.log("handling route user 2")
+  res.send("Response 2");
 
-    
+} );
 
-// app.use('/hello',(req,res)=>{
-//     res.send('hello hello hello!!!')
-// })
-
-// app.use('/',(req,res)=>{
-//   res.send('hello from the dashboard')
-// })
-
-app.get("/user/:userId/:name/:password",(req,res)=>{
-  console.log(req.params)
-  res.send({firstname:"chetan", lastname: "sahu"})
-})
-
-app.post("/user",(req,res)=>{
-  res.send("data is successfully saved in the DB")
-})
 
 app.listen(3000,()=>{
 console.log("server is succersssfully listening in the port no 3000")
